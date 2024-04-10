322aa64509b6268d8838976a1429be02da7d5781
Scott Feichter <scottfeichter@gmail.com>
2024-04-10 16:58:02 -0700

    added css and route handler

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require(`http`);
 const fs = require(`fs`);
 
+const comments = [];
+
 const server = http.createServer((req, res) => {
   if (req.method === `GET` && req.url === `/`) {
     const responseBody = fs.readFileSync(`index.html`, `utf-8`)
 
     res.statusCode = 200;
     res.setHeader(`Content-Type`, `text/html`);
+    return res.end(responseBody);
+  }
+
+  if (req.method === `GET` && req.url === `/static/styles/index.css`) {
+    const responseBody = fs.readFileSync("index.css", "utf-8");
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', `text/css`);
     return res.end(responseBody);
   }
 });

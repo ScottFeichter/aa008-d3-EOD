d05d47568d3ad74fea33ade8815f413afdb39fd7
Scott Feichter <scottfeichter@gmail.com>
2024-04-10 17:20:38 -0700

    added remaining

--- server.js
+++ server.js
@@ -4,17 +4,23 @@ const fs = require(`fs`);
 const comments = [];
 
 const server = http.createServer((req, res) => {
+  let reqBody = "";
+
+  req.on("data", (data) => {
+    reqBody += data;
+  });
+
+  // When the request is finished processing the entire body
 
   req.on("end", () => {
     // Parsing the body of the request
     // x-www-form-urlencoded
     if (reqBody) {
-      //query=mars+rover%21&commit=Search
       req.body = reqBody
-        .split("&") //[query=mars+rover%21,commit=Search]
-        .map((keyValuePair) => keyValuePair.split("=")) //[[query,mars+rover%21],[commit,Search]]
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")]) // [[query,mars rover%21],[commit,Search]]
-        .map(([key, value]) => [key, decodeURIComponent(value)]) // [[query,mars rover!],[commit,Search]]
+        .split("&")
+        .map((keyValuePair) => keyValuePair.split("="))
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+        .map(([key, value]) => [key, decodeURIComponent(value)])
         .reduce((acc, [key, value]) => {
           acc[key] = value;
           return acc;
@@ -29,47 +35,47 @@ const server = http.createServer((req, res) => {
       console.log(req.body);
     }
 
+    // Do not edit above this line
 
+    // route for index.html
+    if (req.method === `GET` && req.url === `/`) {
+      const htmlPage = fs.readFileSync(`index.html`, `utf-8`);
 
+      let commentsList = "";
 
-  // route for index.html
-  if (req.method === `GET` && req.url === `/`) {
-    const htmlPage = fs.readFileSync(`index.html`, `utf-8`)
+      if (!comments.length) commentsList = "<p>No comment created yet</p>";
 
-    let commentsList = ""
+      for (const comment of comments) {
+        commentsList += `<p>${comment}</p>`;
+      }
 
-    for (const comment of comments) {
-      commentsList += `<p>${comment}</p>`
+      const responseBody = htmlPage.replace(/#{comments}/, commentsList);
+
+      res.statusCode = 200;
+      res.setHeader(`Content-Type`, `text/html`);
+      return res.end(responseBody);
     }
 
-    const responseBody = htmlPage.replace(/#{comments}/, commentsList);
+    // route for index.css
+    if (req.method === `GET` && req.url === `/static/styles/index.css`) {
+      const responseBody = fs.readFileSync("index.css", "utf-8");
 
-    res.statusCode = 200;
-    res.setHeader(`Content-Type`, `text/html`);
-    return res.end(responseBody);
-  }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", `text/css`);
+      return res.end(responseBody);
+    }
 
-  // route for index.css
-  if (req.method === `GET` && req.url === `/static/styles/index.css`) {
-    const responseBody = fs.readFileSync("index.css", "utf-8");
+    // route for comment submit
+    if (req.method === `POST` && req.url === `/comments`) {
+      const { comment } = res.body;
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', `text/css`);
-    return res.end(responseBody);
-  }
-
-  // route for comment submit
-  if(req.method === `POST` && req.url === `/comments`) {
-  const { comment } = res.body;
-
-  comments.push(comment);
-  res.statusCode = 302;
-  res.setHeader(`Location`, `/`);
-  console.log(comments);
-  res.end();
-  }
-
-
+      comments.push(comment);
+      res.statusCode = 302;
+      res.setHeader(`Location`, `/`);
+      console.log(`all comments`, comments);
+      return res.end();
+    }
+  });
 });
 
 const port = 5001;
